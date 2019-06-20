17b8c9dee4fa3d42e0cf1d4eccd7b47f7aa272ac
Kai <thiskaiguy@gmail.com>
2019-06-20 11:39:06 +0100

    Remove unused C++/WinRT headers

--- src/pch.h
+++ src/pch.h
@@ -2,12 +2,8 @@
 #pragma comment(lib, "windowsapp")
 
 #include <windows.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.ApplicationModel.Core.h>
 #include <winrt/Windows.UI.Core.h>
-#include <winrt/Windows.UI.Composition.h>
-#include <winrt/Windows.UI.Input.h>
 
 // WinRT ABI
 #include <Windows.ApplicationModel.Core.h>
