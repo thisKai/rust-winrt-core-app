f42aab9d12f484d92effe2f319c2329bb2825449
Kai <thiskaiguy@gmail.com>
2019-06-24 14:28:32 +0100

    Speed up ComProp unsafely

--- src/com.rs
+++ src/com.rs
@@ -1,35 +1,29 @@
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::{
+    cell::UnsafeCell,
+    sync::{Arc, Mutex, MutexGuard}
+};
 
 use winrt::ComPtr;
 
-struct ComPropInner<T>(ComPtr<T>);
-unsafe impl<T> Send for ComPropInner<T> {}
-
-pub struct ComProp<T>(Arc<Mutex<Option<ComPropInner<T>>>>);
+pub struct ComProp<T>(UnsafeCell<Option<ComPtr<T>>>);
 impl<T> ComProp<T> {
     pub fn new(ptr: ComPtr<T>) -> Self {
-        Self(Arc::new(Mutex::new(Some(ComPropInner(ptr)))))
+        Self(UnsafeCell::new(Some(ptr)))
     }
     pub fn get(&self) -> Option<ComPtr<T>> {
-        let lock = self.0.lock().ok()?;
-        lock.as_ref().map(|inner| inner.0.clone())
+        unsafe { &*self.0.get() }.as_ref().cloned()
     }
     pub fn set(&self, value: Option<ComPtr<T>>) {
-        let mut lock = self.0.lock().expect("ComProp set: mutex lock failed");
-        if let Some(current) = &mut *lock {
-            if let Some(new) = value {
-                current.0 = new;
-            } else {
-                *lock = None;
-            }
-        } else {
-            *lock = value.map(|v| ComPropInner(v));
-        }
+        let ptr = unsafe { &mut*self.0.get() };
+        *ptr = value;
     }
 }
+unsafe impl<T> Send for ComProp<T> {}
+unsafe impl<T> Sync for ComProp<T> {}
+
 impl<T> Default for ComProp<T> {
     fn default() -> Self {
-        Self(Arc::new(Mutex::new(None)))
+        Self(UnsafeCell::new(None))
     }
 }
 
